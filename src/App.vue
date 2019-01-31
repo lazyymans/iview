4c8f8ebb098be1ff24df507309c6d36f02cea7ce
luoyc <luoyc@anfawuliu.com>
2019-01-31 13:05:58 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,18 @@ export default {
 }
 </script>
 
-  <style lang="less">
-  .size{
-    width: 100%;
-    height: 100%;
-  }
-  html,body{
-    .size;
-    overflow: hidden;
-    margin: 0;
-    padding: 0;
-  }
-  #app {
-    .size;
-  }
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  .size;
+}
 </style>
